1c39027cd9ab20a999ac59e69792d4362b73c153
michael.J <79923488+michaelafk@users.noreply.github.com>
2023-09-26 13:51:59 +0200

    cambios en los procesos

--- segonintent.py
+++ segonintent.py
@@ -1,35 +1,35 @@
 import time
 import threading
 
-def threads(etiqueta):
-    const_global = 0
-    global wantp
-    global wantq
-    while True:
-        if wantp == False:
-            while(wantq != False):
-                print("proceso 1 esperando")
-        elif wantq == False:
-            while(wantp != False):
-                print("proceso 2 esperando")
-        if wantq == False:
-            wantp = True
-        elif wantp == False:
-            wantq = True
+want = [False,False]
+def proceso_0(etiqueta):
+    global const_global
+    while const_global < 10:
+        while want[1]!=False:
+            print(f" proceso {etiqueta}: esperando")
+        want[0] = True
         const_global+=1
-        if wantq == False:
-            wantp = False
-        elif wantp == False:
-            wantq = False
-        print(f"proceso {etiqueta}: {const_global}")
+        print(f" proceso {etiqueta}: {const_global}")
+        want[0] = False
+def proceso_1(etiqueta):
+    global const_global
+    while const_global < 9:
+        while want[0] != False:
+            print(f" proceso {etiqueta}: esperando")
+        want[1] = True
+        const_global+=1
+        print(f" proceso {etiqueta}: {const_global}")
+        want[1] = False
 
-wantp = False
-wantq = False
-t1 = threading.Thread(target= threads,args=("1",))
-t2 = threading.Thread(target= threads,args=("2",))
+const_global = 0
+t1 = threading.Thread(target= proceso_0,args=("1",))
+t2 = threading.Thread(target= proceso_1,args=("2",))
     
 t1.start()
 t2.start()
+    
+t1.join()
+t2.join()
     
 t1.join()
 t2.join()
