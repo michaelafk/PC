aedc4237a7ef6eae7a9344652d6bcac8d2fbb50f
michael.J <79923488+michaelafk@users.noreply.github.com>
2023-09-26 13:58:24 +0200

    Create tercerintent.py

---
+++ tercerintent.py
@@ -0,0 +1,32 @@
+import time
+import threading
+
+want = [False,False]
+def proceso_0(etiqueta):
+    global const_global
+    while const_global < 9:
+        want[0] = True
+        while want[1]!=False:
+            print(f" proceso {etiqueta}: esperando")
+        const_global+=1
+        print(f" proceso {etiqueta}: {const_global}")
+        want[0] = False
+def proceso_1(etiqueta):
+    global const_global
+    while const_global < 9:
+        want[1] = True
+        while want[0] != False:
+            print(f" proceso {etiqueta}: esperando")
+        const_global+=1
+        print(f" proceso {etiqueta}: {const_global}")
+        want[1] = False
+
+const_global = 0
+t1 = threading.Thread(target= proceso_0,args=("1",))
+t2 = threading.Thread(target= proceso_1,args=("2",))
+    
+t1.start()
+t2.start()
+    
+t1.join()
+t2.join()
