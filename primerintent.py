a002bcf6205ecacc86a79e04ac58d54675f2e907
michael.J <79923488+michaelafk@users.noreply.github.com>
2023-09-26 13:33:41 +0200

    procesos defirentes (proceso_0 y proceso_1)

--- primerintent.py
+++ primerintent.py
@@ -1,27 +1,29 @@
 import time
 import threading
 
-def threads(etiqueta):
+def proceso_0(etiqueta):
+    global const_global
     global turno
-    const_global = 0
     while True:
-        time.sleep(1)
-        if turno == 1:
-            while(turno !=1):
-                print("proceso 1 esperando")
-        else:
-            while(turno !=2):
-                print("proceso 2 esperando")
+        while turno !=1:
+            print(f" proceso {etiqueta}: esperando")
         const_global+=1
-        print(f"proceso {etiqueta}: {const_global}")
-        if turno == 1:
-            turno = 2
-        else:
-            turno = 1
+        print(f" proceso {etiqueta}: {const_global}")
+        turno = 2
+def proceso_1(etiqueta):
+    global const_global
+    global turno
+    while True:
+        while turno !=2:
+            print(f" proceso {etiqueta}: esperando")
+        const_global+=1
+        print(f" proceso {etiqueta}: {const_global}")
+        turno = 1
 
+const_global = 0
 turno = 1
-t1 = threading.Thread(target= threads,args=("1",))
-t2 = threading.Thread(target= threads,args=("2",))
+t1 = threading.Thread(target= proceso_0,args=("1",))
+t2 = threading.Thread(target= proceso_1,args=("2",))
     
 t1.start()
 t2.start()
