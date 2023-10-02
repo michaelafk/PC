90b1c79bfe6a82d674f19bce701009881696408d
michaelafk <onedeath24@gmail.com>
2023-10-02 15:21:51 +0200

    exercici fet

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module example/hellow
+module sample-app
 
 go 1.21.1
---
+++ peterson.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var flag [2]bool
+var last int
+var wg sync.WaitGroup
+var Entrades [2]int
+var entradesTotals int = 5
+
+func Entrada(id int) {
+	defer wg.Done()
+
+	//hacer algo
+	fmt.Printf("Entrada %d\n", id)
+	Entrades[id] = 0
+	for i := 0; i < entradesTotals; i++ {
+		flag[id] = true
+		last = 1 + id
+		var pos int = (id + 1) % 2
+		for flag[pos] || last == 1+id {
+			//espera activa
+		}
+		//seccion critica
+		fmt.Printf("Porta %d: ", id)
+		time.Sleep(time.Millisecond * 500)
+		Entrades[id] += 1
+		fmt.Printf("%d entrades de : %d Temps: %T", Entrades[id], i, time.Now())
+		fmt.Printf("Porta %d\n", id)
+
+		flag[id] = false
+
+		time.Sleep(time.Millisecond * 200)
+	}
+}
+
+func main() {
+	wg.Add(2)
+	go Entrada(0)
+	go Entrada(1)
+	wg.Wait()
+}
