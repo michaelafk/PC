8312941b70e3d503161f54b82dbca5a751167a68
michaelafk <onedeath24@gmail.com>
2023-11-04 13:39:02 +0100

    BASICO practica1

--- practica1/Practica1.py
+++ practica1/Practica1.py
@@ -1,37 +1,57 @@
-import threading
+import threading,time,random
 
-mutex = threading.Semaphore
+#SEMAFOROS
+SemSos = threading.Semaphore(0) #semaforo de los sospechosos
+SemSosE = threading.Semaphore(0) #semaforo de los sospechosos para que vayan acabando
+SemJut = threading.Semaphore(0) #semaforo del juez
+#SemJutS = threading.Semaphore(0)
+#VARIABLES
+SosCount = 0
+#CONSTANTES
 MaxSospitosos = 20
-SemSos = threading.Semaphore()
-SemJut = threading.Semaphore()
-counter = 0
+SosInsala = 6
 def sospitos(id):
-    SemSos.acquire()
-    SemSos.release()
+    global SosCount
+    print(f"sospitos {id}: \tSom innocent!")
+    #time.sleep(random.randint(1,2))
+    
+    SosCount+=1
+    print(f"sospitos {id}: \tha entrat a la sala, suspect {SosCount}")
+    if SosCount == SosInsala:
+        SemJut.release() #hay 6 sospechosos en sala y por tanto llamamos a el juez para que entre
+    SemSosE.acquire()
+    if SosCount == SosInsala:
+        SemJut.release() #acabar jutge
+    print(f"sospitos {id}: \tentra a l'Asil d'Arkham")
 
 def jutge():
-    SemJut.acquire()
-    SemJut.release()
-    SemSos.release()
+    global SosCount
+    SemJut.acquire() #Jutge espera
+    print("Jutge Dredd: Jo som la llei!")
+    for i in range(SosInsala):
+        SemSosE.release()
+    #SemSosE.release() #los procesos sospitosos quedan liberados
+    for i in range(SosInsala):
+        SemJut.acquire()#esperar a  que todos acaben
+    time.sleep(0.1)
+    print("Jutge Dredd: La justícia descansa, demà prendré declaració als sospitosos que queden")
+    
 
 def main():
-    sospitosos = []
-    for i in range(MaxSospitosos):
+    threads = []
+    
+    jut = threading.Thread(target = jutge)
+    threads.append(jut)
+    for i in range(SosInsala):
         sos = threading.Thread(target=sospitos,args=(i+1,))
         sos.name = f"{i+1}"
-        sospitosos.append(sos)
-    for s in sospitosos:
-        s.start()
-        print(f"sospitos {s.name}: \tSom innocent!")
-    jut = threading.Thread(target = jutge)
-    jut.start()
-    print("Jutge Dredd: Jo som la llei!")
+        threads.append(sos)
+        
+    for thread in threads:
+        thread.start()
 
-    for s in sospitosos:
-        s.join()
-        print(f"sospitos {s.name} entra a l'Asil d'Arkham")
-    jut.join()
-    print("Jutge Dredd: La justícia descansa, demà prendré declaració als sospitosos que queden")
+    for thread in threads:
+        thread.join()
 
 
 
