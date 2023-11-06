3a27ca8fec7a2199d496b2a1180d0dac08f8ab8d
michaelafk <onedeath24@gmail.com>
2023-11-06 10:44:51 +0100

    solo queda prints y comentarios

--- practica1/Practica1.py
+++ practica1/Practica1.py
@@ -1,36 +1,41 @@
 import threading,time,random
 
 #SEMAFOROS
-SemSos = threading.Semaphore(0) #semaforo de los sospechosos
+SemSos = threading.Semaphore(1) #semaforo de los sospechosos y para exclusion mutua
 SemSosE = threading.Semaphore(0) #semaforo de los sospechosos para que vayan acabando
 SemJut = threading.Semaphore(0) #semaforo del juez
+turnmutex = threading.Semaphore(1) #semaforo para la exclusion mutua sobre la variable turn
 #SemJutS = threading.Semaphore(0)
 #VARIABLES
 SosCount = 0
+turn = 1
 #CONSTANTES
 MaxSospitosos = 20
 SosInsala = 6
 def sospitos(id):
-    global SosCount
+    global SosCount,turn
     print(f"sospitos {id}: \tSom innocent!")
-    #time.sleep(random.randint(1,2))
-    
+    SemSos.acquire()
     SosCount+=1
-    print(f"sospitos {id}: \tha entrat a la sala, suspect {SosCount}")
+    if turn == 1:
+        print(f"sospitos {id}: \tha entrat a la sala, suspect {SosCount}")
     if SosCount == SosInsala:
         SemJut.release() #hay 6 sospechosos en sala y por tanto llamamos a el juez para que entre
+        turn = 0        #como llamamos al juez y este y inicia, entonces ponemos la puerta cerrada
+    SemSos.release()
     SemSosE.acquire()
     SemJut.release() #acabar jutge
     print(f"sospitos {id}: \tentra a l'Asil d'Arkham")
 
 def jutge():
-    global SosCount
+    global SosCount,turn
     SemJut.acquire() #Jutge espera
     print("Jutge Dredd: Jo som la llei!")
-    for i in range(SosInsala):
+    turn = 0
+    for i in range(MaxSospitosos):
         SemSosE.release()
     #SemSosE.release() #los procesos sospitosos quedan liberados
-    for i in range(SosInsala):
+    for i in range(MaxSospitosos):
         SemJut.acquire()#esperar a  que todos acaben
     time.sleep(0.1)
     print("Jutge Dredd: La justícia descansa, demà prendré declaració als sospitosos que queden")
@@ -41,7 +46,7 @@ def main():
     
     jut = threading.Thread(target = jutge)
     threads.append(jut)
-    for i in range(SosInsala):
+    for i in range(MaxSospitosos):
         sos = threading.Thread(target=sospitos,args=(i+1,))
         sos.name = f"{i+1}"
         threads.append(sos)
