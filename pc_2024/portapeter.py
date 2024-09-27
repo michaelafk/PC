d50dbcbf80779ad73507bdb676035289acaeaff3
michaelafk <onedeath24@gmail.com>
2024-09-27 13:09:36 +0200

    peterson,act 3 hecha

--- pc_2024/portapeter.py
+++ pc_2024/portapeter.py
@@ -1,58 +1,36 @@
-import threading,time
+import threading,time,random
 
-def entrada_0():
+def entrada(id):
     #no critical section
     global want
-    global entries0
+    global entrades
     global Numeroentrades
-    global waittime
-    global waittime2
-    print("Entrada 0")
+    global minwaittime
+    global maxwaittime
+    global last
+    print(f"Entrada {id}")
     for i in range(Numeroentrades):
-        time.sleep(waittime)
-        want[0] = True
-        last = 1
-        while want[1] != False | last == 2:
+        time.sleep(random.uniform(minwaittime,maxwaittime))
+        want[id] = True
+        last = id
+        pos = (id+1) % 2
+        while want[pos] != False | last == pos:
             #esperando
             print()
         #critical section
-        print("Porta 0:")
-        entries0+=1
-        print(f"{entries0} entrades de : {i} Temps: {time.c}")
-        want[0] = False
-        time.sleep(waittime2)
+        entrades[id]+=1
+        print(f"Porta {id}: {entrades[id]} entrades de : {i} Temps: {time.ctime()}")
+        want[id] = False
+        time.sleep(random.uniform(minwaittime,maxwaittime))
 
-def entrada_1():
-    #no critical section
-    global want
-    global entries1
-    global Numeroentrades
-    global waittime
-    global waittime2
-    print("Entrada 1")
-    for i in range(Numeroentrades):
-        time.sleep(waittime)
-        print("")
-        want[1] = True 
-        last = 2
-        while want[0] != False | last == 1:
-            #esperando
-            print()
-        #critical section
-        print("Porta 1: ")
-        entries1+=1
-        print(f"{entries1} entrades de: {i} Temps: x")
-        want[1] = False
-        time.sleep(waittime2)
-
-entries0 = 0
-entries1 = 0
+entrades = [0,0]
 want = [False,False] #wantp = want[0], wantq = want[1]
-Numeroentrades = 20
-waittime = 500/1000
-waittime2 = 200/1000
-p0 = threading.Thread(target = entrada_0)
-p1 = threading.Thread(target = entrada_1)
+Numeroentrades = 10
+minwaittime = 500/1000
+maxwaittime = 5000/1000
+last = 0
+p0 = threading.Thread(target = entrada,args=[0])
+p1 = threading.Thread(target = entrada,args=[1])
 p0.start()
 p1.start()
 p0.join()
